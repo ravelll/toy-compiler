ff8fc5e4a5553ddfea8a63dcb26c73d3f5bda83f
Yoshihide Taniguchi (ravelll) <ravelll.taniguchi@gmail.com>
2019-08-31 17:48:21 +0900

    Fix to take 1 number argument

--- compiler.rb
+++ compiler.rb
@@ -1,4 +1,9 @@
+num = ARGV[0].to_i
+if num.nil?
+  raise StandardError "Nothing was input"
+end
+
 puts "  .global main"
 puts "main:"
-puts "  movq $42, %rax"
+puts "  movq $#{num}, %rax"
 puts "  ret"
