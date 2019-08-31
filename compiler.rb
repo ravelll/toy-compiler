0e3d97079b7002daa076d74070d7cf5cfbffa624
Yoshihide Taniguchi (ravelll) <ravelll.taniguchi@gmail.com>
2019-08-31 23:57:32 +0900

    Add methods get_char / unget_char

--- compiler.rb
+++ compiler.rb
@@ -3,6 +3,23 @@ if num.nil?
   raise StandardError "Nothing was input"
 end
 
+source = []
+source_index = 0
+
+def get_char
+  if source_index == source.size
+    return nil
+  end
+
+  char = source[source_index]
+  source_index += 1
+  char
+end
+
+def unget_char
+  source_index -= 1
+end
+
 puts "  .global main"
 puts "main:"
 puts "  movq $#{num}, %rax"
