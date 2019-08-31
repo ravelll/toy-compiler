77a55ca366e15bb1d017a63264ad8163b9792c34
Yoshihide Taniguchi (ravelll) <ravelll.taniguchi@gmail.com>
2019-09-01 00:02:18 +0900

    Fix implementation to be class-based

--- compiler.rb
+++ compiler.rb
@@ -3,24 +3,34 @@ if num.nil?
   raise StandardError "Nothing was input"
 end
 
-source = []
-source_index = 0
 
-def get_char
-  if source_index == source.size
-    return nil
+class Compiler
+  def initialize(num)
+    @num = num
+    @source = []
+    @source_index = 0
   end
 
-  char = source[source_index]
-  source_index += 1
-  char
+  def get_char
+    if @source_index == @source.size
+      return nil
+    end
+
+    char = @source[@source_index]
+    @source_index += 1
+    char
+  end
+
+  def unget_char
+    @source_index -= 1
+  end
+
+  def output
+    puts "  .global main"
+    puts "main:"
+    puts "  movq $#{@num}, %rax"
+    puts "  ret"
+  end
 end
 
-def unget_char
-  source_index -= 1
-end
-
-puts "  .global main"
-puts "main:"
-puts "  movq $#{num}, %rax"
-puts "  ret"
+Compiler.new(num).output
