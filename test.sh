f4fb17b02c26dc5114b5a64cee474a3b1975e09c
Yoshihide Taniguchi (ravelll) <ravelll.taniguchi@gmail.com>
2019-09-01 18:15:37 +0900

    Add a test script

---
+++ test.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+function unit_test() {
+  local input=$1
+  local expected=$2
+  ruby compiler.rb "$input" > a.s
+  gcc a.s
+  ./a.out
+  local actual=$?
+  if [[ $expected -eq $actual ]];then
+    echo "ok"
+  else
+    echo "not ok : $expected != $actual"
+    exit 1
+  fi
+}
+
+unit_test '42' '42'
+unit_test '+7' '7'
+unit_test ' 7' '7'
+unit_test '7;' '7'
+unit_test ' 7 ;' '7'
+unit_test '-1' '255'
+unit_test '30+12' '42'
+unit_test '6 * 7' '42'
+unit_test '42 / 2' '21'
