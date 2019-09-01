a1beb83b175b6bdeaf07ff1420378a23d5104c81
Yoshihide Taniguchi (ravelll) <ravelll.taniguchi@gmail.com>
2019-09-01 14:37:36 +0900

    Add the Token class

--- compiler.rb
+++ compiler.rb
@@ -1,36 +1,67 @@
-num = ARGV[0].to_i
-if num.nil?
+class Compiler
+  class Runner
+    def initialize(source)
+      @source = source
+      @source_index = 0
+    end
+
+    def read_number(char)
+    end
+
+    def tokenize
+      tokens = []
+      p "# Tokens :"
+
+      loop do
+        char = get_char
+        break unless char
+
+        case char
+        when "0".."9"
+          literal_int = read_number(char)
+          token = Token.new(kind: "literal_int", value: literal_int)
+          tokens = tokens << token
+        else
+          raise StandardError "Tokenizer: Invalid char: '#{char}'"
+        end
+      end
+
+      p "\n"
+      tokens
+    end
+
+    def get_char
+      if @source_index == @source.size
+        return nil
+      end
+
+      char = @source[@source_index]
+      @source_index += 1
+      char
+    end
+
+    def unget_char
+      @source_index -= 1
+    end
+
+    def run
+      num = @source.to_i
+
+      puts "  .global main"
+      puts "main:"
+      puts "  movq $#{num}, %rax"
+      puts "  ret"
+    end
+  end
+
+  class Token
+    attr_accessor :kind, :value
+  end
+end
+
+source = ARGV[0]
+if source.nil?
   raise StandardError "Nothing was input"
 end
 
-
-class Compiler
-  def initialize(num)
-    @num = num
-    @source = []
-    @source_index = 0
-  end
-
-  def get_char
-    if @source_index == @source.size
-      return nil
-    end
-
-    char = @source[@source_index]
-    @source_index += 1
-    char
-  end
-
-  def unget_char
-    @source_index -= 1
-  end
-
-  def output
-    puts "  .global main"
-    puts "main:"
-    puts "  movq $#{@num}, %rax"
-    puts "  ret"
-  end
-end
-
-Compiler.new(num).output
+Compiler::Runner.new(source).run
